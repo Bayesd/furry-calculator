7981db1caad9909a1bfd8946d4386f7267b98710
Leo <gbgleo@gmail.com>
2021-04-28 19:36:51 +0200

    Deleted the class

---
+++ Copy.py
@@ -0,0 +1,6 @@
+class Calc:
+    def add(x, y):
+        return x + y
+
+    def power_of_two(x):
+        return x ** 2    
--- test_Calculator.py
+++ test_Calculator.py
@@ -2,22 +2,21 @@ from Calculator import Calc
 from StringMethods import more_than_nine
 
 
-class Test:
-    def test_add(self):
-        assert 5 == Calc.add(3, 2)
-        assert 19 == Calc.add(8, 11)
+def test_add():
+    assert 5 == Calc.add(3, 2)
+    assert 19 == Calc.add(8, 11)
 
-    def test_power_of_two(self):
-        assert 0 == Calc.power_of_two(0)
-        assert 9 == Calc.power_of_two(3)
-        assert 25 == Calc.power_of_two(5)
+def test_power_of_two():
+    assert 0 == Calc.power_of_two(0)
+    assert 9 == Calc.power_of_two(3)
+    assert 25 == Calc.power_of_two(5)
 
-    def test_modulo(self):
-        assert 2 == Calc.modulo(6, 4)
-        assert 3 == Calc.modulo(8, 5)
+def test_modulo():
+    assert 2 == Calc.modulo(6, 4)
+    assert 3 == Calc.modulo(8, 5)
 
-    def test_more_than_nine(self):
-        assert more_than_nine("Flaggstångsknopp") is True
-        assert more_than_nine("Ord") is False
-        assert more_than_nine(244) is False
-        assert more_than_nine(467382467823) is True
+def test_more_than_nine():
+    assert more_than_nine("Flaggstångsknopp") is True
+    assert more_than_nine("Ord") is False
+    assert more_than_nine(244) is False
+    assert more_than_nine(467382467823) is True
